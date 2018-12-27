006a06e483175258d32f5f362dfb36cddb2692d4
Pravesh <pravesh.humane@gmail.com>
2018-12-27 01:00:59 -0600

    Add files via upload

---
+++ Sorting_algorithms.py
@@ -0,0 +1,62 @@
+# Implementation of MergeSort 
+def mergeSort(arr): 
+    if len(arr) >1: 
+        mid = len(arr)/2 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        mergeSort(L) # Sorting the first half 
+        mergeSort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+          
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+
+
+
+
+# Implementation of Quicksort Sort 
+
+def partition(arr,low,high): 
+    i = ( low-1 )         # index of smaller element 
+    pivot = arr[high]     # pivot 
+  
+    for j in range(low , high): 
+  
+        if   arr[j] <= pivot: 
+          
+            # increment index of smaller element then swap
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+  
+  
+def quickSort(arr,low,high): 
+    if low < high: 
+  
+        pi = partition(arr,low,high) 
+  
+        # sort elements before partition and after partition 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
